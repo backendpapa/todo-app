01501644e6fe759831a689492539879e07b56688
Akeju Paul <akejupaul10@gmail.com>
2021-12-28 03:36:55 +0100

    info-swipe-layout

--- App.js
+++ App.js
@@ -31,25 +31,32 @@ const App = () => {
 
 
   return (
-    <SafeAreaView style={{display:'flex',flex:1}}   >
-      <View style={{backgroundColor:'#FEFEFF',flex:1,justifyContent:'center'}}>
-      <View style={{backgroundColor:'#FEFEFF',flex:1,justifyContent:'center',padding:30}} >
-      
-      <Image
-        style={{height:200,width:200}}
-        source={require('./assets/task1.png')}
-      />
-      <Text style={{fontFamily:'sefoe',fontWeight:'bold',fontSize:30,color:'#25306B'}}>
-        Get things done
-      </Text>
-      <Text style={{color:'#D7D8D9',fontSize:20,fontWeight:'bold',marginRight:100}}>
-        Just a click away from planning your tasks
-      </Text>
+    <SafeAreaView style={{ display: 'flex', flex: 1 }}   >
+      <View style={{ backgroundColor: '#FEFEFF', flex: 1, justifyContent: 'center' }}>
+        <View style={{ backgroundColor: '#FEFEFF', flex: 1, justifyContent: 'center', padding: 30 }} >
 
-      </View>
-      <View style={{position:'absolute',bottom:-180,right:-200,borderRadius:500,backgroundColor:'#25306B',width:400,height:400}}>
-       
-      </View>
+          <Image
+            style={{ height: 250, width: 250 }}
+            source={require('./assets/task1.png')}
+          />
+          <Text style={{ fontFamily: 'sefoe', fontWeight: 'bold', fontSize: 30, color: '#25306B' }}>
+            Get things done
+          </Text>
+          <Text style={{ color: '#D7D8D9', fontSize: 20, fontWeight: 'bold', marginRight: 100 }}>
+            Just a click away from planning your tasks
+          </Text>
+
+        </View>
+        <View style={{ position: 'absolute', bottom: -180, right: -200, borderRadius: 500, backgroundColor: '#25306B', width: 400, height: 400 }}>
+        <TouchableOpacity  activeOpacity={0.5} style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+    <Image
+     source={require('./assets/arrow-right2.png')}
+     style={{marginLeft:-110,marginTop:-130}}
+    />
+ 
+   
+</TouchableOpacity>
+        </View>
       </View>
 
     </SafeAreaView>
